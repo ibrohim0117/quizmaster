ad7dc6fb226f222e865c21979bb3d91d89c93e16
ibrohim <ibrohim.dev.uz@gmail.com>
2026-01-06 22:44:36 +0500

    .

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import User, UserConfirmation, Achievement
 
 
-admin.site.register(User)
-admin.site.register(UserConfirmation)
+# admin.site.register(User)
+# admin.site.register(UserConfirmation)
 admin.site.register(Achievement)
+
+
+@admin.register(UserConfirmation)
+class UserConfirmationAdmin(admin.ModelAdmin):
+    list_display = ['code', 'expiration_time', 'is_confirmed', 'user', 'id']
+    search_fields = ['is_confirmed', ]
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'levels', 'roles', 'is_verified', 'is_superuser', 'ball', 'id']
+    search_fields = ['username', ]
+
